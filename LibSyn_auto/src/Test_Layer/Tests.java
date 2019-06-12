f6ddfe29a59095ce3395f18697db4079d51c97ef
mabhatt <mabhatt@CIPL-1PC37.cygnet.com>
2019-06-12 13:24:09 +0530

    Parameterized app url

--- LibSyn_auto/src/Test_Layer/Tests.java
+++ LibSyn_auto/src/Test_Layer/Tests.java
@@ -5,16 +5,19 @@ import java.io.IOException;
 import org.testng.annotations.Test;
 
 import Framework_Layer.Framework_Methods;
+import Framework_Layer.Properties_Reader;
 
 
 public class Tests {
 
+Framework_Layer.Properties_Reader reader1 = new Properties_Reader();
 	
 	@Test
 	public void Access_Site() throws IOException 
 	{
 		// TODO Auto-generated method stub
-		Framework_Methods.OpenSite("http://www.google.com");
+		String URL = reader1.getPropValues("ApplicationURL");
+		Framework_Methods.OpenSite(URL);
 	
 	}
 
